ed51a7814d2a6076e4d53769d1652bbda596bb3a
raz.kaziro <razkaziro@gmail.com>
2019-11-03 19:13:41 +0200

    Added MyStrCpy

--- projects/class/cw2/cw2main.c
+++ projects/class/cw2/cw2main.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-int MyStrcmp(const char *s1, const char *s2);
+int MyStrCmp(const char *s1, const char *s2);
 
 int main(void)
 {
 
-char str1[10]="a";
-char str2[10]="A";
+int x;
 
-printf("str2-vs-str1 %d \n", MyStrcmp(str1,str2));
+/* Testing strcmp*/
+
+char str1[20]="Hello Worla";
+char str2[20]="Hello Worlb";
+
+/*printf("str2-vs-str1 %s \n", strcpy(dest,src));*/
+printf("str2-vs-str1 %d \n", strcmp(str1, str2));
+
 }
 
-
- int MyStrcmp(const char *s1, const char *s2)
+ int MyStrCmp(const char *s1, const char *s2)
  {
 
  	char const *runner1 = s1, *runner2 = s2;
- 	int ret, tmpsts = 0;
-
- 	while ('\0'!=*runner1 || '\0'!=*runner2 )
+ 	
+ 	while (*runner1 == *runner2)
  	{
- 		if(*runner1 > *runner2)
- 		{
- 			tmpsts = *runner1 - *runner2;
- 			++runner1;
- 			++runner2;
- 		}
- 		else
- 		{
- 			tmpsts = *runner1 - *runner2;
- 			++runner1;
- 			++runner2;
- 		}
+		++runner1;
+	 	++runner2;
  	}
- 	return tmpsts;
+ return *runner1-*runner2;
  }
+
 
