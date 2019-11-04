014f78bdbcbf786edea2293cc7bdcea082a0b5b2
raz.kaziro <razkaziro@gmail.com>
2019-11-04 22:47:13 +0200

    adding .myfuncs

--- c/class/ws2/ws2main.c
+++ c/class/ws2/ws2main.c
@@ -15,7 +15,7 @@ char ch = '2';
 
 printf("MyStrChr %s \n", MyStrChr(str1, ch));
 printf("strchr %s \n", strchr(str1, ch));
-
+ks
 /*printf("MyStrNCpy %d \n", MyStrCaseCmp(str1, str2));*/
 /*printf("strcasecmp %d", strcasecmp(str1,str2));*/
 
