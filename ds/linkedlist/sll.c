563d448d988d4a62c5f63349d30fb133f63c80d1
raz.kaziro <razkaziro@gmail.com>
2019-12-03 19:21:25 +0200

    ready for review

--- ds/linkedlist/sll.c
+++ ds/linkedlist/sll.c
@@ -1,19 +1,23 @@
 /*
  * Author: Raz KaziRo
  * Purpose: Answares for DS - Single Linked List.
- * Date: 02.12.2019
+ * Date: 03.12.2019
  * Language: C
- * Reviewer: 
+ * Reviewer: Itai Marienberg
  */
+
 #include <stdlib.h> /*malloc()*/
 #include <assert.h> /*assert()*/
 
-#include "sll.h"
+#include "sll.h" /*Single Linked List API Functions*/
 
 node_t *SLLCreateNode(node_t *node, const void *data)
 {	
 	node_t *newNode = (node_t *)malloc(sizeof(node_t));
-	if(NULL != newNode)
+
+	assert (NULL != data);
+
+	if (NULL != newNode)
 	{
 		newNode->data = (void *)data;
 		newNode->next = node;
@@ -23,59 +27,26 @@ node_t *SLLCreateNode(node_t *node, const void *data)
 
 void SLLDestroy(node_t *current_node)
 {	
-	node_t *next_runner = current_node->next;
-	node_t *temp = NULL;
+   node_t *tmp_current = NULL;
 
-	while (NULL != next_runner)
-	{	
-		temp = next_runner;
-		next_runner = next_runner->next;
-		free(temp);
-	}
-	current_node->next = NULL;
-	free(current_node);
-	current_node = NULL;
+   assert (NULL != current_node);
+
+   while (NULL != current_node)
+    {
+       tmp_current = current_node;
+       current_node = current_node->next;
+       free(tmp_current);
+    }
 }
 
-void SLLRemoveNode(node_t *current_node)
-{	
-	node_t *next_node = current_node->next;
-
-	assert (NULL != current_node);
-
-	while (NULL != next_node)
-	{
-		current_node->data = next_node->data;
-		current_node->next = next_node->next;
-		free(next_node);
-		next_node = NULL;
-	}
-}
-/*
-void SLLDestroy(node_t **node)
-{	
-	node_t *tmp1 = NULL;
-	node_t *tmp2 = NULL; 
-	tmp1 = *node;
-
-	assert(NULL != node);
-
-	while (NULL != tmp1)
-	{
-		tmp2 = tmp1->next;
-		free(tmp1);
-		tmp1 = tmp2;
-	}
-	free(*node);
-	*node = NULL;
-}
-*/
-
-/* Node A Before Node B */
-int InsertBefore(node_t *node_b, node_t *node_a)
+int SLLInsert(node_t *node_a, node_t *node_b)
 {
-	void *tmp_data = node_a->data;
+	void *tmp_data = NULL;;
 
+	assert (NULL != node_a);
+	assert (NULL != node_b);
+
+	tmp_data = node_a->data;
 	node_a->next = node_b->next;
 	node_a->data = node_b->data;
 	node_b->data = tmp_data;
@@ -94,8 +65,13 @@ int SLLInsertAfter(node_t *new_node, node_t *previous_node)
 
 size_t SLLSize(const node_t *head)
 {	
-	node_t *node_runner = (node_t *)head; 
+	node_t *node_runner = NULL;
 	size_t count = 0;
+
+	assert (NULL != head);
+
+	node_runner = (node_t *)head;
+
 	while (NULL != node_runner)
 	{	
 		node_runner = node_runner->next;
@@ -105,8 +81,160 @@ size_t SLLSize(const node_t *head)
 	return count;
 }
 
-void SLLRemoveAfter(node_t *node)
-{
-	node_t *next_node = node->next;
-	*node->next = *next_node->next;
+void SLLRemoveNode(node_t *current_node)
+{	
+	node_t *next_node = NULL;
+
+	assert (NULL != current_node);
+
+	next_node = current_node->next;	
+
+	while (NULL != next_node)
+	{
+		current_node->data = next_node->data;
+		current_node->next = next_node->next;
+		free(next_node);
+		next_node = NULL;
+	}
 }
+
+void SLLRemoveAfter(node_t *current_node)
+{	
+	node_t *next_node = NULL;
+
+	assert(NULL != current_node);
+
+	if (NULL != current_node->next)
+	{
+		next_node = current_node->next;
+		current_node->next = next_node->next;
+		free(next_node);
+		next_node = NULL;
+	}
+}
+
+node_t *SLLGetNode(const node_t *head, match_func match_ptr, void *additional)
+{
+	node_t *node_runner = NULL;
+
+	assert(NULL != head);
+	assert(NULL != match_ptr);
+	assert(NULL != additional);
+
+	node_runner = (node_t *)head;
+
+	while (0 == match_ptr(node_runner, additional))
+	{
+		node_runner = node_runner->next;
+	}
+	return node_runner;
+}
+
+int SLLForEach(node_t *head, action_func action_ptr, void *additional)
+{	
+	node_t *node_runner = NULL;
+
+	assert(NULL != head);
+	assert(NULL != action_ptr);
+	assert(NULL != additional);
+
+	node_runner = (node_t *)head;
+
+	while (0 == action_ptr(node_runner, additional) && NULL != node_runner)
+	{
+		node_runner = node_runner->next;
+	}
+
+	return !(NULL == node_runner);
+}
+
+node_t *SLLFlip(node_t *head)
+{
+	node_t *current_node = NULL; 
+	node_t *next_node = NULL;
+	node_t *prev_node = NULL;
+
+	assert (NULL != head);
+
+	current_node = head;
+
+	while (NULL != current_node)
+	{
+		next_node = current_node->next;
+		current_node->next = prev_node;
+		prev_node = current_node;
+		current_node = next_node;
+	}
+	head = prev_node;
+
+	return head;
+}
+
+int SLLHasLoop(const node_t *head)
+{		
+	const node_t *slow_p = NULL, *fast_p = NULL;
+
+	assert (NULL != head);
+
+	slow_p = head;
+	fast_p = head->next;
+
+    while (slow_p && fast_p && fast_p->next && (slow_p != fast_p)) 
+    { 
+        slow_p = slow_p->next; 
+        fast_p = fast_p->next->next;
+    }
+
+    return(slow_p == fast_p);
+}
+
+node_t *SLLFindIntersection(const node_t *head1, const node_t *head2)
+{
+	const node_t *head1_runner = NULL;
+	const node_t *head2_runner = NULL;
+
+	size_t size1 = 0;
+	size_t size2 = 0;
+	size_t steps = 0, i = 0;
+
+	assert (NULL != head1);
+	assert (NULL != head2);
+
+	head1_runner = head1;
+	head2_runner = head2;
+	size1 = SLLSize(head1);
+	size2 = SLLSize(head2);
+
+	steps = size1 > size2 ? (size1 - size2) : (size2 - size1);
+
+	if (size1 > size2)
+	{
+		for(; i<steps; ++i)
+		{
+			head1_runner = head1->next;
+		}
+	}
+	else
+	{
+		for (; i<steps; ++i)
+		{
+			head2_runner = head2->next;
+		}
+	}
+
+	while (NULL != head1_runner && NULL != head2_runner)
+	{
+		if (head1_runner == head2_runner)
+		{
+			return (node_t *)head1_runner;
+		}
+
+		head1_runner = head1_runner->next;
+		head2_runner = head2_runner->next;
+	}
+
+	return (node_t *)head1_runner;
+}
+
+
+
--- ds/linkedlist/sll_test.c
+++ ds/linkedlist/sll_test.c
@@ -1,44 +1,443 @@
 /*
  * Author: Raz KaziRo
  * Purpose: Answares for DS - Single Linked List.
- * Date: 02.12.2019
+ * Date: 03.12.2019
  * Language: C
- * Reviewer: 
+ * Reviewer: Itai Marienberg
  */
 
-#include <stdio.h>
+#include <stdio.h> /*printf()*/
 #include <stdlib.h> /*free()*/
-#include "sll.h"
 
+#include "sll.h" /*Single Linked List API Functions*/
+
+#define RUN_TEST(test, errmsg)\
+							{\
+								if(test)\
+								{\
+									printf("SUCCESS\n");\
+								}\
+								else\
+								{\
+									printf("\033[1;31m");\
+									printf("%s \n", errmsg);\
+									printf("\033[0m");\
+								}\
+							}
 #define UNUSED(x) (void)(x)
+#define FREE(ptr) free(ptr); ptr=NULL;							
 
+static int isMatch(void *node, void *additional)
+{
+	node_t *check_node = (node_t*)node;
 
-int main(int argc, char const *argv[])
+	if(*(int *)additional == (*(int *)check_node->data))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+static int addOne(void *node, void *additional)
+{
+	node_t *node_runner = (node_t*)node;
+
+	if(NULL != node_runner)
+	{
+		*(int *)node_runner->data += *(int *)additional;
+	}
+	return (NULL == node_runner);
+}
+
+void TestSLLCreateNode()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	int a = 10;
+	int b = 20;
+	n2 = SLLCreateNode(NULL,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	RUN_TEST(a == *(int *)n1->data, "FAIL: Wrong Data (10)");
+	RUN_TEST(n2 == n1->next, "FAIL: n1->next(NULL)");
+	RUN_TEST(b == *(int *)n2->data, "FAIL: Wrong Data (20)");
+	RUN_TEST(NULL == n2->next, "FAIL: n2->next(n1)");
+
+	FREE(n1);
+	FREE(n2);
+
+	printf("\n");
+}
+
+void TestSLLDestroy()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *head = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+
+	n3 = SLLCreateNode(NULL,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+	head = n1;
+
+	RUN_TEST(n2 == n1->next, "FAIL: n2->next(n3)");
+	RUN_TEST(n3 == n2->next, "FAIL: n2->next(n3)");
+	RUN_TEST(NULL == n3->next, "FAIL: n3->next(NULL)");
+	SLLDestroy(head);
+
+	printf("\n");
+}
+
+void TestSLLInsert()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n3 = SLLCreateNode(NULL,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+	n4 = SLLCreateNode(NULL,&d);
+
+	SLLInsert(n4,n2);
+
+	RUN_TEST(b == *(int *)n4->data, "FAIL: Wrong Data (b)");
+	RUN_TEST(d == *(int *)n2->data, "FAIL: Wrong Data (d)");
+	RUN_TEST(c == *(int *)n3->data, "FAIL: Wrong Data (c)");
+	RUN_TEST(a == *(int *)n1->data, "FAIL: Wrong Data (a)");
+	RUN_TEST(n3 == n4->next, "FAIL: n4->next (n2)");
+	RUN_TEST(NULL == n3->next, "FAIL: n3->next (NULL)");
+	RUN_TEST(n4 == n2->next, "FAIL: n2->next (n3)");
+	RUN_TEST(n2 == n1->next, "FAIL: n1->next (n4)");
+	RUN_TEST(4 == SLLSize(n1), "FAIL: Size (4)");
+	SLLDestroy(n1);
+
+	printf("\n");
+
+}
+void TestSLLInsertAfter()
+{	
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n3 = SLLCreateNode(NULL,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+	n4 = SLLCreateNode(NULL,&d);
+
+	SLLInsertAfter(n4,n2);
+	RUN_TEST(d == *(int *)n4->data, "FAIL: Wrong Data (d)");
+	RUN_TEST(NULL == n3->next, "FAIL: n3->next (NULL)");
+	RUN_TEST(n3 == n4->next, "FAIL: n4->next (n3)");
+	RUN_TEST(n4 == n2->next, "FAIL: n2->next (n4)");
+	RUN_TEST(n2 == n1->next, "FAIL: n1->next (n2)");
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+void TestSLLSize()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n3 = SLLCreateNode(NULL,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+	RUN_TEST(3 == SLLSize(n1), "FAIL: Wrong Size (3)");
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3->next = n4;
+	RUN_TEST(4 == SLLSize(n1), "FAIL: Wrong Size (4)");
+	RUN_TEST(NULL == n4->next, "FAIL: n4->next (NULL)");
+	RUN_TEST(n4 == n3->next, "FAIL: n3->next (NULL)");
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+void TestSLLRemoveNode()
 {
 	node_t *n1 = NULL;
 	node_t *n2 = NULL;
 	node_t *n3 = NULL;
 
-	int a = 10;	
+	int a = 10;
 	int b = 20;
 	int c = 30;
 	
-	n1 = SLLCreateNode(NULL, &a);
-	n2 = SLLCreateNode(NULL, &b);
-	n3 = SLLCreateNode(NULL, &c);
+	n3 = SLLCreateNode(NULL,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
 
-	SLLInsertAfter(n2,n1);
-	SLLInsertAfter(n3,n2);
+	RUN_TEST(3 == SLLSize(n1), "FAIL: Wrong Size (3)");
+	SLLRemoveNode(n2);
+	RUN_TEST(2 == SLLSize(n1), "FAIL: Wrong Size (2)");
+	RUN_TEST(NULL == n2->next, "FAIL: n2->next (NULL)");
+	RUN_TEST(n2 == n1->next, "FAIL: n1->next (n2)");
+	RUN_TEST(c == *(int *)n2->data, "FAIL: Wrong Data (c)");
+	RUN_TEST(a == *(int *)n1->data, "FAIL: Wrong Data (c)");
 
-	printf("n1 data:%d\n",*(int *)n1->data);
-	printf("n2 data:%d\n",*(int *)n2->data);
-	printf("n3 data:%d\n",*(int *)n3->data);
-
-	printf("before: %lu \n", SLLSize(n1));
 	SLLDestroy(n1);
-	/*printf("before: %lu \n", SLLSize(n1));*/
 
-	/*printf("%d", *(int *)n2->data);*/
+	printf("\n");
+
+}
+
+void TestSLLRemoveAfter()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	RUN_TEST(4 == SLLSize(n1), "FAIL: Wrong Size (4)");
+	RUN_TEST(n3 == n2->next, "FAIL: n2->next (n3)");
+	SLLRemoveAfter(n2);
+	RUN_TEST(3 == SLLSize(n1), "FAIL: Wrong Size (3)");
+	RUN_TEST(n4 == n2->next, "FAIL: n2->next (n4)");
+	RUN_TEST(b == *(int *)n2->data, "FAIL: n2->data (b)");
+	RUN_TEST(d == *(int *)n4->data, "FAIL: n2->data (d)");
+
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+void TestSLLGetNode()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+	node_t *result = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	result = SLLGetNode(n1, &isMatch, &b);
+	RUN_TEST(n2 == result, "FAIL: n2 == result");
+	result = SLLGetNode(n1, &isMatch, &d);
+	RUN_TEST(n4 == result, "FAIL: n4 == result");
+	RUN_TEST(4 == SLLSize(n1), "FAIL: Wrong Size (4)");
+
+	SLLDestroy(n1);
+
+	printf("\n");
+
+}
+
+void TestSLLForEach()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+	int add = 5;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	RUN_TEST(a == *(int *)n1->data, "FAIL: n1->data (a+5)");
+	RUN_TEST(b == *(int *)n2->data, "FAIL: n2->data (b+5)");
+	RUN_TEST(c == *(int *)n3->data, "FAIL: n3->data (c+5)");
+	RUN_TEST(d == *(int *)n4->data, "FAIL: n4->data (d+5)");
+
+	SLLForEach(n1, &addOne, &add);
+
+	RUN_TEST(15 == *(int *)n1->data, "FAIL: n1->data (a+5)");
+	RUN_TEST(25 == *(int *)n2->data, "FAIL: n2->data (b+5)");
+	RUN_TEST(35 == *(int *)n3->data, "FAIL: n3->data (c+5)");
+	RUN_TEST(45 == *(int *)n4->data, "FAIL: n4->data (d+5)");
+
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+void TestSLLFlip()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+	node_t *head = n1;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	head = SLLFlip(n1);
+	RUN_TEST(head == n4, "FAIL: head == n4");
+	RUN_TEST(n3 == n4->next, "FAIL: n4->next (n3)");
+	RUN_TEST(n2 == n3->next, "FAIL: n3->next (n2)");
+	RUN_TEST(n1 == n2->next, "FAIL: n2->next (n1)");
+	RUN_TEST(NULL == n1->next, "FAIL: n1->next (NULL)");
+
+	SLLDestroy(n4);
+
+	printf("\n");
+}
+
+void TestSLLHasLoop()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+	n4->next = n2;
+
+	RUN_TEST(1 == SLLHasLoop(n1), "FAIL: Has Loop");
+	n4->next = NULL;
+	RUN_TEST(0 == SLLHasLoop(n1), "FAIL: No LOOP");
+
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+void TestSLLFindIntersection()
+{
+	node_t *n1 = NULL;
+	node_t *n2 = NULL;
+	node_t *n3 = NULL;
+	node_t *n4 = NULL;
+
+	node_t *n5 = NULL;
+	node_t *n6 = NULL;
+	node_t *n7 = NULL;
+	node_t *result = NULL;
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+	int e = 50;
+	int f = 60;
+	int g = 70;
+
+	n4 = SLLCreateNode(NULL,&d);
+	n3 = SLLCreateNode(n4,&c);
+	n2 = SLLCreateNode(n3,&b);
+	n1 = SLLCreateNode(n2,&a);
+
+	n7 = SLLCreateNode(n3,&g);
+	n6 = SLLCreateNode(n7,&f);
+	n5 = SLLCreateNode(n6,&e);
+
+	result = SLLFindIntersection(n1,n5);
+
+	RUN_TEST(n3 == result, "FAIL: n3 == result");
+	n7->next = NULL;
+	SLLDestroy(n5);
+	SLLDestroy(n1);
+
+	printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+
+	printf("TestSLLCreateNode()\n");
+	TestSLLCreateNode();
+
+	printf("TestSLLDestroy()\n");
+	TestSLLDestroy();
+
+	printf("TestSLLInsert()\n");
+	TestSLLInsert();
+
+	printf("TestSLLInsertAfter()\n");
+	TestSLLInsertAfter();
+
+	printf("TestSLLSize()\n");
+	TestSLLSize();
+
+	printf("TestSLLRemoveNode()\n");
+	TestSLLRemoveNode();
+
+	printf("TestSLLRemoveAfter()\n");
+	TestSLLRemoveAfter();
+
+	printf("TestSLLGetNode()\n");
+	TestSLLGetNode();
+
+	printf("TestSLLForEach()\n");
+	TestSLLForEach();	
+
+	printf("TestSLLFlip()\n");
+	TestSLLFlip();
+
+	printf("TestSLLHasLoop()\n");
+	TestSLLHasLoop();
+
+	printf("TestSLLFindIntersection()\n");
+	TestSLLFindIntersection();
 
 	UNUSED(argc);
 	UNUSED(argv);
