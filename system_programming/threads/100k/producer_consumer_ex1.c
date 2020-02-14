16e0f538e8fec9951ebd025d7f4f4ed5713ad1df
raz.kaziro <razkaziro@gmail.com>
2020-02-14 23:52:21 +0200

    pthread

--- system_programming/threads/100k/producer_consumer_ex1.c
+++ system_programming/threads/100k/producer_consumer_ex1.c
@@ -5,50 +5,47 @@
 #define ARR_START_LOCATION 0
 
 int g_int_arr[NUM_OF_INTS] = {0};
-volatile int g_lock = 0;
+int g_lock = 0;
 
 void *SetArray(void *param)
 {	
-	size_t i = 0;
-	size_t j = 0;
-	while (__sync_lock_test_and_set(&g_lock, 0));
+	size_t i = 0, j = 0;
 
-	if(!g_lock)
+	for(i = 0; i < NUM_OF_INTS; ++i)
 	{
-		for(; i < NUM_OF_INTS; ++i)
+		/*while (1 == g_lock){}*/
+		while(__sync_lock_test_and_set (&g_lock, 1));
+
+		for(j = 0; j < NUM_OF_INTS; ++j)
 		{
-			for(;j < NUM_OF_INTS; ++j)
-			{
-				g_int_arr[i] += j;
-			}
+			g_int_arr[j] = i;
 		}
+		__sync_lock_release(&g_lock);
 		g_lock = 1;
 	}
-
-	__sync_lock_release(&g_lock);
 
 	return NULL;
 }
 
 void *GetArray(void *param)
 {
-	size_t i = 0;
-	size_t j =0;
+	size_t i = 0, j = 0;
 
-	while (__sync_lock_test_and_set(&g_lock, 1));
-
-	if(g_lock)
+	for(j = 0; j < NUM_OF_INTS - 1; ++j)
 	{
-		for(; i < NUM_OF_INTS; ++i)
+		/*while (0 == g_lock){}*/
+		while(__sync_lock_test_and_set (&g_lock, 0));
+		
+		for(i = 0 ;i < NUM_OF_INTS; ++i)
 		{
-			for(; j < NUM_OF_INTS; ++j)
-			{
-				printf("%d", g_int_arr[i]);
-			}
+			printf("%d", g_int_arr[i]);
 		}
-	g_lock = 0;
+		printf("\n");
+		__sync_lock_release(&g_lock);
+		g_lock = 1;
+
 	}
-	__sync_lock_release(&g_lock);
+
 	return NULL;
 }
 
@@ -57,9 +54,8 @@ int main(int argc, char const *argv[])
 
 	pthread_t producer_tread;
 	pthread_t consumer_tread;
-	size_t i = 0;
 
-
+	
 	pthread_create(&producer_tread, NULL, &SetArray, NULL);
 	pthread_create(&consumer_tread, NULL, &GetArray, NULL);
 
