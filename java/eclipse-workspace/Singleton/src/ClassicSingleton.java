a08591cef2b7fee557d456f887ba0741c231342e
raz.kaziro <razkaziro@gmail.com>
2020-03-16 14:57:22 +0200

    Singleton

---
+++ java/eclipse-workspace/Singleton/src/ClassicSingleton.java
@@ -0,0 +1,14 @@
+
+public class ClassicSingleton {
+	
+	//1. Classic Java Singleton pattern
+    private static ClassicSingleton instance;
+    private ClassicSingleton (){}
+ 
+    public static ClassicSingleton getInstance() {
+    if (instance == null) { //not thread-safe
+        instance = new ClassicSingleton();
+    }
+    return instance;
+    }
+}
---
+++ java/eclipse-workspace/Singleton/src/UltimateSingleton.java
@@ -0,0 +1,12 @@
+
+public class UltimateSingleton {
+ 
+        private static class SingletonHolder {
+           public static UltimateSingleton instance = new UltimateSingleton();
+        }
+     
+        public static UltimateSingleton getInstance() {
+            return SingletonHolder.instance;
+        }
+}
+
