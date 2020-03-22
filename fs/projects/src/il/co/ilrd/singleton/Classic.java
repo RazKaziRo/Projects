a5f824e91e35120dd45db6c13d3fd0b285a0816d
Raz KaziRo <Raz KaziRo@DESKTOP-4K607JA>
2020-03-22 11:20:45 +0200

    singleton
    Former-commit-id: ca186b27d75318ac850687dd9372976d979af69a [formerly f9a8139ee84713be2d85f8af417c2318b79d56dc]
    Former-commit-id: 9c30ac038bc68c76afcc66575b812285b5c59a1e

--- fs/projects/src/il/co/ilrd/singleton/Classic.java
+++ fs/projects/src/il/co/ilrd/singleton/Classic.java
@@ -3,14 +3,18 @@ package il.co.ilrd.singleton;
 
 public class Classic {
 	
-	//1. Classic Java Singleton pattern
     private static Classic instance;
-    private Classic (){}
+    
+    private Classic () {
+    	
+    }
  
     public static Classic getInstance() {
-    if (instance == null) { //not thread-safe
-        instance = new Classic();
-    }
-    return instance;
+    	
+    	if (instance == null) { //not thread-safe
+    		instance = new Classic();
+    	}
+    	
+    	return instance;
     }
 }
--- fs/projects/src/il/co/ilrd/singleton/SingletonTest.java
+++ fs/projects/src/il/co/ilrd/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import il.co.ilrd.singleton.Enum.Singleton;
+
 public class SingletonTest {
 
 	@Test
@@ -53,5 +55,39 @@ public class SingletonTest {
 		System.out.println("Ultimate Singleton 3 HashCode: " + singleton3.hashCode());
 		System.out.println();
 	}
+	
+	@Test
+	public void EnumTest() {
+		
+		 Singleton singleton1 = Enum.Singleton.SINGLETON;
+		 Singleton singleton2 = Enum.Singleton.SINGLETON;
+		 Singleton singleton3 = Enum.Singleton.SINGLETON;
+		 
+		assertEquals(singleton1, singleton2);
+		assertEquals(singleton1, singleton3);
+		
+		System.out.println("Enum Singleton 1 HashCode: " + singleton1.hashCode());
+		System.out.println("Enum Singleton 2 HashCode: " + singleton2.hashCode());
+		System.out.println("Enum Singleton 3 HashCode: " + singleton3.hashCode());
+		System.out.println();
+		
+	}
+	
+	@Test
+	public void ThreadSafeTest() {
+		
+		ThreadSafe singleton1 = ThreadSafe.getInstance();
+		ThreadSafe singleton2 = ThreadSafe.getInstance();
+		ThreadSafe singleton3 = ThreadSafe.getInstance();
+		
+		assertEquals(singleton1, singleton2);
+		assertEquals(singleton1, singleton3);
+		
+		System.out.println("ThreadSafe Singleton 1 HashCode: " + singleton1.hashCode());
+		System.out.println("ThreadSafe Singleton 2 HashCode: " + singleton2.hashCode());
+		System.out.println("ThreadSafe Singleton 3 HashCode: " + singleton3.hashCode());
+		System.out.println();
 
+	}
+	
 }
