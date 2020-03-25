57105b6cb8f1f90afd4bd9f8a9f22efb8c8219c9
raz.kaziro <razkaziro@gmail.com>
2020-03-25 11:32:04 +0200

    removed brainfuel
    Former-commit-id: de71d38c71335f87f7eab80bcc899983fc3b2136 [formerly 0084f9647bf071d393d399200efdc1ab214625d1]
    Former-commit-id: 9fba1e7525207b489bac1220a33a7ef6a5f72bd1 [formerly 10a8b613b99420c4147ca18a19c62ab7c55c6635]
    Former-commit-id: fd5c96a9e7a663074f14797bbc96bbd7e2fca189
    Former-commit-id: 5871d3b7428267a88b109f2bc31d2ac4562666d8

--- brain_fuel/uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-del "%AppData%\Microsoft\Windows\Start Menu\Programs\Brain Fuel.lnk"
-
-set folder="%~dp0" 
-cd /d %folder% 
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
