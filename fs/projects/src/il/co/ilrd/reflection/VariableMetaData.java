81efdce8a268fe616c1f26c7223a89f814f63625
raz.kaziro <razkaziro@gmail.com>
2020-03-30 16:23:52 +0300

    reflection ex
    Former-commit-id: b4979b727d9d659cdab00918966eee1d6c0beda8

---
+++ fs/projects/src/il/co/ilrd/reflection/VariableMetaData.java
@@ -0,0 +1,10 @@
+package il.co.ilrd.reflection;
+
+public class VariableMetaData {		
+	
+    public static int IntVarA = 1111;
+    static int IntVarB = 2222;	
+    
+    static String StringVarA="StringVarA";							
+    static String StringVarB = "StringVarB";    
+}	
