ee6df6c670f4da14834e7e7174628035620fd2d0
raz.kaziro <razkaziro@gmail.com>
2020-03-31 18:05:52 +0300

    java2c after review
    Former-commit-id: a319714bbd51b1db052f0f2181e998a2af775908 [formerly d360f9ad5b6f77868a78c3d07ed7136ec2335d5d]
    Former-commit-id: 939e0f3f02653c50c3282c78ae1a1441d429823b

--- c/recursion/recursion.h
+++ c/recursion/recursion.h
@@ -11,5 +11,9 @@ int RecursiveStrCmp(const char *s1, const char *s2);
 char *RecursiveStrCpy(char *dest, const char *src);
 char *RecursiveStrCat(char *dest, const char *src);
 char *RecursiveStrStr(const char *haystack, const char *needle);
+int recMultiply(int num, int times);
+int recDivision(int num, int times);
+unsigned int BitWiseAddition(unsigned int x, unsigned y);
+
 
 #endif
