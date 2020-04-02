52d14b44b659e5ff2be02dff4b51569fbb2e5e45
raz.kaziro <razkaziro@gmail.com>
2020-04-02 17:51:53 +0300

    generics ex
    Former-commit-id: aad7712d95b5b0af4695a751b651a7c15dd64abf [formerly 38bf68a8e7702985733191a5a4b607b418d3e0ed]
    Former-commit-id: bf09b1da3647500f5799085b6a02efcd094e6adc

---
+++ fs/projects/src/il/co/ilrd/generics/Exercises.java
@@ -0,0 +1,66 @@
+package il.co.ilrd.generics;
+
+public class Exercises {
+	
+	public class FooReference<T> {
+		
+		private T t;
+		 
+		public FooReference(T t) {
+			this.t = t;
+			
+		}
+			
+		public T getT() {
+			return t;
+		}
+
+		public void setT(T t) {
+			this.t = t;
+		}
+	
+		
+		public Class<?> getClassAtRunTime() {
+			return t.getClass();
+		}
+		
+	}
+	
+	public static <T> void printArray(T[] array) {
+		
+		for(T element : array) {
+			System.out.println(element);
+		}
+		System.out.println();
+	}
+	
+	
+	public static void main(String[] argv) {
+		
+		
+		
+		Exercises ex = new Exercises();
+		Integer [] ints = {1,2,3,4,5};
+		Character [] chars = {'a','b', 'c', 'd', 'e'};
+		Boolean [] bools = {true, false, true, false, false};
+		
+		printArray(ints);
+		printArray(chars);
+		printArray(bools);
+		
+		FooReference<Integer> intRef = ex.new FooReference<Integer>(1);
+		FooReference<String> strRef = ex.new FooReference<String>("Hello");
+		
+		System.out.println("intRef.getT() " + intRef.getT());
+		System.out.println(intRef.getClassAtRunTime());
+		intRef.setT(6);
+		System.out.println("intRef.getT() " + intRef.getT());
+		
+		System.out.println("strRef.getT() " + strRef.getT());
+		System.out.println(strRef.getClassAtRunTime());
+		strRef.setT("World");
+		System.out.println("f.getT() " + strRef.getT());
+
+}
+	
+}
